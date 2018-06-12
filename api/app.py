bcee53e93cbff5aa9616e043eae50d126db62be9
Thomas Robinson <tdr@thomasr.co>
2018-06-12 18:03:12 +1200

    remove data loading functions from api

--- api/app.py
+++ api/app.py
@@ -2,17 +2,9 @@
 from flask import Flask, jsonify
 import redis
 
-from majestic import load_majestic
-from dynamicdns import load_dynamicdns
-
 # init flask and redis
 app = Flask(__name__)
 r = redis.StrictRedis(host='redis', port=6379, db=0)
-
-# init data in redis
-load_majestic(r, filename='data/majestic_million.csv')
-load_dynamicdns(r, filename='data/dynamic-dns.txt')
-
 
 @app.route('/majestic/<domain>')
 def majestic(domain):
